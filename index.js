803ced30607889998b4142e17397b1d361c52fa6
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-11-06 20:31:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var obj = { recipes: 'bar'};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObj = Object.assign({}, object)
+
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+
+
+}
+
+function deleteFromObjectByKey(object, key){
+
+
+}
