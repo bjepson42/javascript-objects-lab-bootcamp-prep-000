5c97dc4d97cb065a0f238699470e4c765858ccab
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-11-06 20:34:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var recipes = Object.assign({})
 
 function updateObjectWithKeyAndValue(object, key, value){
   var newObj = Object.assign({}, object, {key, value})
-
+  return newObj
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
