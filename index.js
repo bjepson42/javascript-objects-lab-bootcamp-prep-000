dbedc54d802548e80bbf0f696d182d874715c5b7
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-11-06 20:41:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-
-
+  object[key] = value
+  return object
 }
 
 function deleteFromObjectByKey(object, key){
+
+}
+
+function destructivelyDeleteFromObjectByKey(object,key){
 
 
 }
